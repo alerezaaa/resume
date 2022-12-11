0032aa4e6b76a48e5a7a23d197720620f0f66b50
Alireza Dehghanzadeh <86428901+alerezaaa@users.noreply.github.com>
2022-12-11 14:31:34 +0330

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,31 +1,13 @@
-import { Router } from '@layer0/core'
+import { Router } from '@edgio/core'
+
 
 const ONE_DAY = 60 * 60 * 24
+
 const FAR_FUTURE = ONE_DAY * 365 * 10
 
+
 export default new Router()
-  // Prevent search engine bot(s) from indexing
-  // Read more on: https://docs.layer0.co/guides/cookbook#blocking-search-engine-crawlers
-  .noIndexPermalink()
-  .match('/assets/:path*', ({ serveStatic, cache }) => {
-    cache({
-      browser: {
-        maxAgeSeconds: ONE_DAY,
-      },
-      edge: {
-        maxAgeSeconds: ONE_DAY,
-        staleWhileRevalidateSeconds: FAR_FUTURE,
-      },
-    })
-    serveStatic('_site/assets/:path*')
-  })
-  .match('/:path*', ({ serveStatic, cache }) => {
-    cache({
-      browser: false,
-      edge: {
-        maxAgeSeconds: ONE_DAY,
-        staleWhileRevalidateSeconds: FAR_FUTURE,
-      },
-    })
-    serveStatic('_site/:path*')
-  })
+
+  // Create serveStatic route for each file in the folder _site with a cache-control header of 's-maxage=315360000'
+
+  .static('_site')
